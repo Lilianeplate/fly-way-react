476b60b380eb75c686adf7e4d1db82ddc4c03428
lilianeplate <liliane.plate@gmail.com>
2025-02-17 08:39:04 -0300

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,54 @@
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import styles from './Header.module.css';  // Importação do arquivo CSS Module
+
+// function Header () {
+//   return (
+//     <div className={styles.headerContainer}>
+//     <div className={styles.headerContent}>
+//         <img src="src/assets/FlyWay-p.png" alt="" />
+//         <input type="search" placeholder='O que você procura?' />
+//         <button>Quem somos?</button>
+//         <button>Objetivos</button>
+//         <button>Fale com a gente</button>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Header
+
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlane, faSearch, faUsers, faComment } from '@fortawesome/free-solid-svg-icons';
+import styles from './Header.module.css';  // Importação do CSS Module
+
+function Header() {
+  return (
+    <div className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="src/assets/FlyWay-p.png" alt="Logo" />
+        
+        {/* Container do Input e Ícone */}
+        <div className={styles.searchContainer}>
+          <FontAwesomeIcon icon={faSearch} className={styles.searchIcon} />
+          <input type="search" placeholder="O que você procura?" className={styles.searchInput} />
+        </div>
+
+        <button>
+        <FontAwesomeIcon icon={faUsers} />
+          Quem somos
+        </button>
+        <button>
+        <FontAwesomeIcon icon={faPlane}/>
+          Objetivos
+        </button>
+        <button>
+          <FontAwesomeIcon icon={faComment} />
+          Fale com a gente
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
